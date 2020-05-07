747e81329a844a1f52b05bbbb64502e158cb09a9
SanhoLee <stlsh0516@gmail.com>
2020-05-07 22:58:15 +0900

    2.3 Chat with SocketIO

--- src/server.js
+++ src/server.js
@@ -23,6 +23,19 @@ const io = socketIO.listen(server);
 // localhost:4000/socket.io/socket.io.js
 // 이 코드가, 백엔드와 프런트엔드를 소통하는 역할을 함.
 
+// socket.on 의 개념은 특정 이벤트를 계속 listening하고 있다고 이해하면 쉽다.
+// broad method도 event emit 하는것과 같은데, 연결된 socket이외의 client들에게 모두 이벤트를 전송할 때 사용.
+// 즉, 한 클라이언트로부터 메세지를 전송받으면 그 메세지를 나머지 클라이언드들한테 바로 new message이벤트로 전송하면, 나머지 클라이언트 들이 new message이벤트를 listen 하고있다면, real-time chat이 가능하게 됨.
+
 io.on("connection", (socket) => {
-  socket.on("helloGuys", () => console.log("the clien said Hello"));
+  socket.on("newMessage", ({ message }) => {
+    // receive message and broadcast message to another socket client.
+    socket.broadcast.emit("messageNotif", {
+      message,
+      nickname: socket.nickname || "Anon",
+    });
+  });
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+  });
 });
--- src/static/index.js
+++ src/static/index.js
@@ -1,5 +1,20 @@
 // eslint-disable-next-line no-undef
 const socket = io("/");
 
-socket.on("hello", () => console.log("Somdbody Joined!"));
-setTimeout(() => socket.emit("helloGuys"), 4000);
+// console에서 아래 함수를 실행시키면, 서버로 전송되고 메세지를 보내거나 닉네임을 설정한다.
+// emit(event, data) 형태로, 백엔드 서버로 데이터도 같이 전송한다.
+function sendMessage(message) {
+  socket.emit("newMessage", { message });
+  console.log(`You : ${message}`);
+}
+
+function setNickname(nickname) {
+  socket.emit("setNickname", { nickname });
+}
+
+function handleMessageNotif(data) {
+  const { message, nickname } = data;
+  console.log(`${nickname} : ${message}`);
+}
+
+socket.on("messageNotif", handleMessageNotif);
