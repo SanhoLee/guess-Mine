c76fcfc611eb2d297f07220c95986728109210dd
SanhoLee <stlsh0516@gmail.com>
2020-05-10 22:08:16 +0900

    3.2 Browserify part One

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,8 @@ import autoprefixer from "gulp-autoprefixer";
 // autoprefixer는 컴파일된 css 파일 안에서, webkit prefix 등을 추가해주는 라이브러리임.
 // 여러개의 브라우저에서 호환가능하게 해줌.
 import minifyCSS from "gulp-csso";
+import bro from "gulp-browserify";
+import del from "del";
 
 sass.compiler = require("node-sass");
 
@@ -14,10 +16,20 @@ const paths = {
     watch: "assets/scss/**/*.scss",
     // watch를 해당 경로의 모든 scss확장자를 갖는 파일로 지정
   },
+  js: {
+    src: "assets/js/main.js",
+    dest: "src/static/js",
+    watch: "assets/js/**/*.js",
+  },
 };
 
-function styles() {
-  return gulp
+const clean = () => del(["src/static"]);
+
+const js = () =>
+  gulp.src(paths.js.src).pipe(bro()).pipe(gulp.dest(paths.js.dest));
+
+const styles = () =>
+  gulp
     .src(paths.styles.src)
     .pipe(sass())
     .pipe(
@@ -27,13 +39,13 @@ function styles() {
     )
     .pipe(minifyCSS())
     .pipe(gulp.dest(paths.styles.dest));
-}
 
-function watchFile() {
+const watchFile = () => {
   gulp.watch(paths.styles.watch, styles);
-}
+  gulp.watch(paths.js.watch, js);
+};
 
-const dev = gulp.series([styles, watchFile]);
+const dev = gulp.series(clean, styles, js, watchFile);
 
 export default dev;
 // export default하면, gulpfile을 실행시키는걸로 바로 , dev를 실행시키도록 되어있음.
