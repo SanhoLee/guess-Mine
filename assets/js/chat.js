fff4484c29e0f215565cba38e3f748431ff54b2b
SanhoLee <stlsh0516@gmail.com>
2020-05-15 08:38:54 +0900

    5.3 Chat part Two

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,9 +1,11 @@
+import { getSocket } from "./sockes";
+
 const message = document.getElementById("jsMessage");
 const sendMsg = document.getElementById("jsSendMsg");
 
 const appendMsg = (text, nickname) => {
   const li = document.createElement("li");
-  li.innerHTML = `<span "authon ${nickname ? "out" : "self"}">${
+  li.innerHTML = `<span class="author ${nickname ? "out" : "self"}">${
     nickname ? nickname : "You "
   }:</span> ${text}`;
   message.appendChild(li);
@@ -13,8 +15,13 @@ const handleSendMsg = (event) => {
   event.preventDefault();
   const input = sendMsg.querySelector("input");
   const { value } = input;
+  getSocket().emit(window.events.sendMsg, { message: value });
   input.value = "";
   appendMsg(value);
+};
+
+export const handleNewMessage = ({ message, nickname }) => {
+  appendMsg(message, nickname);
 };
 
 if (sendMsg) {
